d23c624b65ed7c1508cb5b6e0deb09de91807243
arjun_raf <arjunhornet@gmail.com>
2024-01-17 22:38:10 +0100

    Finsihed ANLA Assignment 6

--- ANLA scripts/Assignment6/anlaassg6.py
+++ ANLA scripts/Assignment6/anlaassg6.py
@@ -7,7 +7,8 @@ Created on Thu Jan 11 14:55:12 2024
 import numpy as np
 from scipy.linalg import hilbert
 import matplotlib.pyplot as plt
-A = hilbert(4)
+#A = hilbert(4)
+A = np.diag([1, 2, 3, 4]) + np.ones((4, 4))
 
 def gershgorin(A):
     """Function that returns the maximum and minimum eigen value estimates 
@@ -32,28 +33,75 @@ def gershgorin(A):
             
         if Lambda_max == None or gershgorin_dict[key]["Xmax"] > Lambda_max:
             Lambda_max = gershgorin_dict[key]["Xmax"]
-        print("Current min: " + str(Lambda_min))
-        print("Current max: " + str(Lambda_max))
+        # print("Current min: " + str(Lambda_min))
+        # print("Current max: " + str(Lambda_max))
     
-    return [Lambda_min,Lambda_max]
+    return Lambda_min,Lambda_max
 
 def power(A,v0):
     """Function to implement power iteration to find eigen values of a matrix"""
-    m,_ = np.shape(A)
-    v = np.zeros((m,1)) 
-    v[0] = 1 #Initial guess Vo
-    conv = 1
+    conv = 1 #Just a dummy value above the convergence criterion to get started with the loop
     error_list = []
+    v = v0
     while conv > 1e-13:
         w = np.dot(A,v)
         v_k = w/(np.linalg.norm(w,ord=2))
         Av_k = np.dot(A, v_k)
         lambda_k = np.dot(v_k.T,Av_k)
-        diff = np.dot(A,v_k) - lambda_k*v_k
-        conv = max(np.abs(diff))
-        conv = conv[0] #since I don't need an array but a number
+        diff = Av_k - lambda_k*v_k
+        conv = np.max(np.abs(diff))
+        #conv = conv[0] #since I don't need an array but a number
         error_list.append(conv)
         v = v_k #v_k would be the new eigen vector estimation
     return v,lambda_k,error_list
-        
-        
+
+def inverse(A,v0,mu):
+    """Function to implement inverse iteration to find eigen values of a matrix"""
+    conv = 1 #Just a dummy value above the convergence criterion to get started with the loop
+    error_list = []
+    v = v0
+    m,_ = np.shape(A)
+    while conv > 1e-13:
+        A_muI = A - mu*np.eye(m)
+        A_mu_inv = np.linalg.inv(A_muI)
+        w = np.dot(v,A_mu_inv)
+        v_k = w/np.linalg.norm(w,ord=2)
+        Av_k = np.dot(A,v_k)
+        lambda_k = np.dot(v_k.T,Av_k)
+        diff = Av_k - lambda_k*v_k
+        conv = np.max(np.abs(diff))
+        #conv = conv[0]
+        error_list.append(conv)
+        v = v_k
+    return v,lambda_k,error_list
+
+def rayleigh(A,v0):
+    """Function to implement Rayleigh Quotient iteration to find eigen values of a matrix"""
+    conv = 1 #Just a dummy value above the convergence criterion to get started with the loop
+    error_list = []
+    v = v0
+    Av0 = np.dot(A,v0)
+    lambda_0 = np.dot(v0.T,Av0)
+    lambda_k = lambda_0
+    m,_ = np.shape(A)
+    while conv > 1e-13:
+        A_lambdak = A - lambda_k*np.eye(m)
+        A_lambdak_inv = np.linalg.inv(A_lambdak)
+        w = np.dot(v,A_lambdak_inv)
+        v_k = w/np.linalg.norm(w,ord=2)
+        Av_k = np.dot(A,v_k)
+        lambda_k = np.dot(v_k.T,Av_k)
+        diff = Av_k - lambda_k*v_k
+        conv = np.max(np.abs(diff))
+        error_list.append(conv)
+        v = v_k
+    return v,lambda_k,error_list
+
+
+def randomInput(m):
+    #! DO NOT CHANGE THIS FUNCTION !#
+    A = np.random.rand(m, m) - 0.5
+    A += A.T  # make matrix symmetric
+    v0 = np.random.rand(m) - 0.5
+    v0 = v0 / np.linalg.norm(v0) # normalize vector
+    return A, v0    
--- ANLA scripts/Assignment6/convergence_comparison.py
+++ ANLA scripts/Assignment6/convergence_comparison.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as pl
 
 # import eigenvalues_musterlsg as ev
-import eigenvalues as ev
+import anlaassg6 as ev
 
 def convergence_comparison():
     # fix seed for reproducibility
@@ -20,24 +20,24 @@ def convergence_comparison():
     μ_rand = (np.random.rand(1) - 0.5) * np.sqrt(m) * 6
 
     # run all algorithms
-    _, λp, conv_p = ev.power(A, v0)
+    #_, λp, conv_p = ev.power(A, v0)
     _, λir, conv_ir = ev.inverse(A, v0, μ_rand)
     _, λig, conv_ig = ev.inverse(A, v0, μ_gershgorin)
-    _, λr, conv_r = ev.rayleigh(A, v0)
+    #_, λr, conv_r = ev.rayleigh(A, v0)
 
     # print results
     print(f'algorithm           |   λ')
     print('--------------------------------------------')
-    print(f'power               | {λp}')
+    #print(f'power               | {λp}')
     print(f'inverse (random μ)  | {λir}')
     print(f'inverse (Gershgorin)| {λig}')
-    print(f'Rayleigh quotient   | {λr}')
+    #print(f'Rayleigh quotient   | {λr}')
 
     # plot convergence
-    pl.semilogy(range(len(conv_p)), conv_p, ".-", label="power iteration")
+    #pl.semilogy(range(len(conv_p)), conv_p, ".-", label="power iteration")
     pl.semilogy(range(len(conv_ir)), conv_ir, ".-", label="inverse iteration (random μ)")
     pl.semilogy(range(len(conv_ig)), conv_ig, ".-", label="inverse iteration (Gershgorin μ)")
-    pl.semilogy(range(len(conv_r)), conv_r, ".-", label="Rayleigh iteration")
+    #pl.semilogy(range(len(conv_r)), conv_r, ".-", label="Rayleigh iteration")
     pl.xlabel("iterations")
     pl.ylabel("error")
     pl.legend()
