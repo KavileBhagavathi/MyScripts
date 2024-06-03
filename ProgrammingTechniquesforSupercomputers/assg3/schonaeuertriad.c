fc115795803d0ddb4a1a2e02ea071e2f3ed8bdd0
arjun_raf <arjunhornet@gmail.com>
2024-06-03 22:35:16 +0200

    Added Assignment 3 solutions and plots

---
+++ ProgrammingTechniquesforSupercomputers/assg3/schonaeuertriad.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+double getTimeStamp();
+int main()
+{
+    double wct_start,wct_end;
+    float performance;
+    FILE *fptr;
+    for(int filenum=1;filenum<6;filenum++) //Loop to run multiple benchmark runs and generate individual op files
+    {
+        char filename[sizeof "schonauertriad_benchmark00.txt"];
+        sprintf(filename, "schonauertriad_benchmark%02d.txt", filenum);
+        fptr = fopen(filename,"w");
+        fprintf(fptr,"Array_Length,Total_WallTime,NIter,Performance(F/s)\n");  
+        for(int m=8;m<=44;m++)
+        {
+            int N = (int)pow((double) 1.5,m);
+            //printf("%d,",N);
+            double *a = malloc(N*sizeof(double));  
+            double *b = malloc(N*sizeof(double));
+            double *c = malloc(N*sizeof(double));
+            double *d = malloc(N*sizeof(double));
+
+            if ((a == NULL) || (b == NULL) || (c == NULL) || (d == NULL))
+                {
+                    printf("Memory not allocated.\n");
+                    exit(0);
+                }
+            else
+                {   
+                    for (int i=0; i<N; i++)
+                    {
+                        a[i] = 1.0;
+                        b[i] = 1.0;
+                        c[i] = 1.0;
+                        d[i] = 1.0;
+                    }
+
+                    int NITER=1;
+                    do {
+                        // time measurement
+                        wct_start = getTimeStamp();
+
+                        // repeat measurement often enough
+                        for(int k=0; k<NITER; ++k) 
+                        {
+                            // This is the benchmark loop
+                            for(int i=0; i<N; ++i) 
+                            {
+                                // put loop body here: a[i] = ...
+                                a[i] = b[i] + c[i] * d[i];
+                            }
+                            // end of benchmark loop
+                        if(a[N/2]<0.) printf("%lf",a[N/2]); // prevent compiler from eliminating loop
+                        }
+                        wct_end = getTimeStamp();
+                        NITER = NITER*2;
+                    } while (wct_end-wct_start<0.1); // at least 100 ms
+
+                    NITER = NITER/2;
+                    performance = (2.0*NITER*N)/(wct_end-wct_start);
+                    //printf("Total walltime: %f, NITER: %d\n",wct_end-wct_start,NITER);
+                    fprintf(fptr,"%d,",N);
+                    fprintf(fptr,"%f,",wct_end-wct_start);
+                    fprintf(fptr,"%d,",NITER);
+                    fprintf(fptr,"%f\n",performance);
+
+
+                }
+            free (a);
+            free (b);
+            free (c);
+            free (d);
+            
+        }
+        fclose(fptr);
+    }
+
+}
+
+
